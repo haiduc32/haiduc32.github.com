2db8e0eaadb2c771b912e4a961833d0e8a0ab717
Radu Pascal <haiduc32@gmail.com>
2014-08-17 00:22:35 +0300

    small updates

--- import.ps1
+++
@@ -1,9 +0,0 @@
-ruby -rubygems -e 'require "jekyll-import";
- JekyllImport::Importers::WordPress.run({
-  "dbname" => "blog_cyberkinetx",
-   "user"   => "root",
-   "password" => "MyNewPass",
-   "host"    => "partizan-server",
-   "table_prefix" => "wp_angt54_",
-   "comments" => true
- })'
